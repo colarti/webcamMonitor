ba9cf6f8f7c84d107f5cb70c956a71694c2a2a77
cmolarti <cmolarti@gmail.com>
2023-12-29 00:48:46 -0500

    send email when detected, then changed to send email after object leaves the scene

---
+++ sendMessage.py
@@ -0,0 +1,4 @@
+import smtplib, requests
+
+def send_email(img):
+    print(f'email was sent')
--- webcam.py
+++ webcam.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import time
+from sendMessage import send_email
 
 
 #initialize camera
@@ -8,8 +9,9 @@ cam = cv2.VideoCapture(0)   #0 is the intenral integrated camera
 time.sleep(1)
 
 first_frame = None
-
+status_list = list()
 while True:
+    status = 0
     check, frame = cam.read()
     grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     grey_frame_gau = cv2.GaussianBlur(grey_frame, (11, 11), 0)
@@ -27,10 +29,19 @@ while True:
             if cv2.contourArea(contour) < 5000:
                 continue
             x,y,w,h = cv2.boundingRect(contour)
-            cv2.rectangle(frame, (x,y), (x+w,y+h), (0, 255, 0), 3)
+            rectangle = cv2.rectangle(frame, (x,y), (x+w,y+h), (0, 255, 0), 3)
+            if rectangle.any():
+                status = 1
+    
+        status_list.append(status)  # detecting motion or no motion
+        status_list = status_list[-2:]  # just capture the last values
 
+        if status_list[0] == 1 and status_list[1] == 0: #this tracks when an object is detected then leaves the scene
+            send_email(frame)
 
         cv2.imshow('Camera Video', frame)
+
+
 
 
 
